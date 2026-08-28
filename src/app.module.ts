79a6b4962649ee02317bb3796581ce01a5a0e9f6
ggnika1337 <nikushanika568@gmail.com>
2026-08-28 13:23:59 +0400

    feat:logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: { singleLine: true },
+        },
+      },
+    }),
 
     MongooseModule.forRoot(process.env.MONGO_URL as string),
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import * as dns from 'dns';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 // error fix
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
